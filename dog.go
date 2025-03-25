ac639e90597f239fa17ed39c6aa26b537c745c10
rzcastilho <rodrigo.zampieri@gmail.com>
2025-03-25 14:47:56 -0300

    dog new feature

---
+++ dog.go
@@ -0,0 +1,7 @@
+package dog
+
+import "strings"
+
+func WhenGrowsUp(s string) string {
+	return "When the puppy grows up it says: " + strings.ToUpper(s)
+}
---
+++ go.mod
@@ -0,0 +1,3 @@
+module github.com/rzcastilho/dog
+
+go 1.24.1

aac082ee36ac1e415353afdcfd6d29c9e0038fc2
rzcastilho <rodrigo.zampieri@gmail.com>
2025-04-04 23:15:52 -0300

    dog docs

--- dog.go
+++ dog.go
@@ -1,7 +1,16 @@
+// Package dog provides functions to dealing with dogs
 package dog
 
-import "strings"
+import (
+	"fmt"
+	"strings"
+)
 
 func WhenGrowsUp(s string) string {
 	return "When the puppy grows up it says: " + strings.ToUpper(s)
+}
+
+// Years returns dog years based on human years
+func Years(i int) int {
+	return i / 7
 }
